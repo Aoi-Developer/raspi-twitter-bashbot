e197ced64df01540985483a1c68f588f16626ba1
あおい <lzuoteng99@gmail.com>
2023-03-30 00:24:04 +0900

    TwitterAPI v2に対応
    改行コードを適切に処理できるように修正。osモジュールの使用を廃止(subprocessへ変更)

---
+++ commandstart.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+#[twitterID]にTwitterアカウントのIDを入れてください(例:sed -e 's/@Chromium_Linux//g')
+
+account=$(echo -n @ && sed -n 1P setting.txt)
+
+str1=$( echo "$1" | sed -e 's/シェル//g' | sed -e 's/'$account'//g' | tr -d '\n' | sed -e 's/  //g')
+
+str2=$(eval timeout 4 chroot --userspec=test ../chroot $str1 2>&1)
+
+echo "$str2" | sed -z 's@\n@\\n@g' | cut -c 1-140 | sed -z 's@\\n@\n@g'
--- tweet.py
+++ tweet.py
@@ -1,14 +1,14 @@
 import tweepy
-import os
 import re
+import subprocess
 import json
 
-API_KEY = os.popen('sed -n 2P setting.txt').readline().strip()
-API_SECRET = os.popen('sed -n 3P setting.txt').readline().strip()
-ACCESS_TOKEN = os.popen('sed -n 4P setting.txt').readline().strip()
-ACCESS_TOKEN_SECRET = os.popen('sed -n 5P setting.txt').readline().strip()
-BEARER_TOKEN = os.popen('sed -n 6P setting.txt').readline().strip()
-TWITTER_ID = os.popen('sed -n 1P setting.txt').readline().strip()
+API_KEY = subprocess.run(['sed', '-n', '2P', 'setting.txt'],capture_output=True,text=True).stdout.replace("\n", "")
+API_SECRET = subprocess.run(['sed', '-n', '3P', 'setting.txt'],capture_output=True,text=True).stdout.replace("\n", "")
+ACCESS_TOKEN = subprocess.run(['sed', '-n', '4P', 'setting.txt'],capture_output=True,text=True).stdout.replace("\n", "")
+ACCESS_TOKEN_SECRET = subprocess.run(['sed', '-n', '5P', 'setting.txt'],capture_output=True,text=True).stdout.replace("\n", "")
+BEARER_TOKEN = subprocess.run(['sed', '-n', '6P', 'setting.txt'],capture_output=True,text=True).stdout.replace("\n", "")
+TWITTER_ID = subprocess.run(['sed', '-n', '1P', 'setting.txt'],capture_output=True,text=True).stdout.replace("\n", "")
 
 Client = tweepy.Client(bearer_token=BEARER_TOKEN, consumer_key=API_KEY, consumer_secret=API_SECRET, access_token=ACCESS_TOKEN, access_token_secret=ACCESS_TOKEN_SECRET)
 
@@ -24,10 +24,9 @@ class IDPrinter(tweepy.StreamingClient):
   tweet_id = (tweet.id)
 
   if 'シェル' in name:
-   cmdcode = 'sh commandstart.sh ' +  '"' + (name) + '"'
-   current_time = os.popen(cmdcode).readline().strip()
-   print(current_time)
-   Client.create_tweet(text=current_time,in_reply_to_tweet_id=tweet_id)
+   current_time = subprocess.run(['bash' , 'commandstart.sh' , name], stdout=subprocess.PIPE, text=True)
+   print(current_time.stdout.strip())
+   Client.create_tweet(text=current_time.stdout.strip(),in_reply_to_tweet_id=tweet_id)
   else:
     print("このリプライは返信対象ではないためスキップします")
 
@@ -36,6 +35,6 @@ def main():
     #printer.add_rules(tweepy.StreamRule(TWITTER_ID))
     printer.add_rules(tweepy.StreamRule("to:" + (TWITTER_ID) + "シェル"))
     printer.filter()
-    
+
 if __name__ == '__main__':
     main()
